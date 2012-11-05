e7d663d67b92b26889949570147e86d7652a6c17
Roy Solhaugen <roysolhaugen@gmail.com>
2012-11-05 11:34:08 +0100

    En början
    Ett tomt skal til tema bara att börja s fort vi vet hur det ska se ut

---
+++ wp-content/themes/Rag/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<title>RAG</title>
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/common.js" ></script>	
+</head>
+<body>
